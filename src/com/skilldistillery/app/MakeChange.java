32361f6f101a7affba5f213a088707c8f6b0fa14
Cullen Rafferty <cullen1882@gmail.com>
2022-08-19 21:27:03 -0400

    method makechange logic

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -12,7 +12,7 @@ public class MakeChange {
 		System.out.println("Please enter the amount tendered by the customer: ");
 		double payment = scan.nextDouble();
 		double changeToCust = change(itemPrice, payment);
-		
+		System.out.println(changeToCust);
 		
 	}
 	public static void welcome() {
@@ -26,14 +26,37 @@ public class MakeChange {
 		System.out.println("***** ALWAYS MAKE SURE ******");
 		System.out.println("********* TO COUNT **********");
 		System.out.println("******** YOUR DRAWER ********");
+		System.out.println("*****************************");
 	}
 	public static double change(double cost, double payAmount) {
+		double changeToCust = 0.0;
+		
+		if (payAmount < cost) {
+			System.out.println("Pay up sucka! You're a few coins short!");
+		}
+		else if (cost < 20 && cost > 10) {
+			changeToCust = cost - payAmount;
+			System.out.println("10 or less");
+		}
+		else if (cost >= 5.0) {
+			changeToCust = cost - payAmount;
+			System.out.println("5 or less");
+		}
+		else if (cost <= 1.0) {
+			changeToCust = cost - payAmount;
+			System.out.println("1 or less");
+		}
+		else {
+			
+		}
+		
+			
 		
 		
 	
 	
 	
-	return ;
+	return changeToCust;
 	}
 	
 	
